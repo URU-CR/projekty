205c1b6863fc56e1baac26668d95798391fbd8d0
aleskrupa109 <ales.krupa@gmail.com>
2026-09-21 15:00:55 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Doplňte hodnoty ze Supabase → Project Settings → API
 const CONFIG = {
   SUPABASE_URL: 'https://cwrsfuwypfycbtmzamus.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_Cyo_R94ia4hhVZvrPHBwOQ_b3Kc'
+  SUPABASE_ANON_KEY: 'sb_publishable_Cyo_R94ia4hhVZvrPHBwOQ_b3Kc-5wg'
 };
